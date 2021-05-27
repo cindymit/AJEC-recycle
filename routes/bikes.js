a19197e899de3e7f82dd23f11b389ca0f176f878
ealtidor <emanuella.altidor@gmail.com>
2021-05-27 17:48:21 -0400

    Created new backend file structure

---
+++ routes/bikes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import * as controllers from '../controllers/bikes.js'
+
+
+const router = Router()
+
+
+router.get('/', (req, res) => res.send("This is root!"))
+
+router.get('/bikes', controllers.getBikes)
+router.get('/bikes/:id', controllers.getBike)
+router.post('/bikes', controllers.createBike)
+router.put('/bikes/:id', controllers.updateBike)
+router.delete('/bikes/:id', controllers.deleteBike)
+
+export default router
--- server.js
+++ server.js
@@ -1,73 +1,27 @@
+import db from './db/connection.js'
+import routes from './routes/bikes.js'
+
 import express from 'express'
 import logger from 'morgan'
 import cors from 'cors'
 
+const app = express()
 const PORT = process.env.PORT || 3001
 
-import db from './db/connection.js'
-import Post from './models/post.js'
-
-const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(logger('dev'))
+
+app.use('/api', routes)
 
 db.on('connected', () => {
   console.log('Connected to MongoDB!')
   app.listen(PORT, () =>
-    console.log(`Express server application is running on port ${PORT}`)
-  )
+  process.env.NODE_ENV === 'production'
+  ? console.log(`Express server running in production on port ${PORT}\n\n`) 
+    : console.log(`Express server running in development on: http://localhost:${PORT}`))
 })
 
-app.get('/', (req, res) => res.send("This is root!"))
 
-app.get('/posts', async (req, res) => {
-    try {
-        const posts = await Post.find()
-        res.json(posts)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
 
-app.get('/posts/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const post = await Post.findById(id)
-        if (!post) throw Error('Post not found')
-        res.json(post)
-    } catch (e) {
-        console.log(e)
-        res.send('Post not found!')
-    }
-})
-
-app.post('/posts', async (req, res) => {
-    try {
-        const post = await new Post(req.body)
-        await post.save()
-        res.status(201).json(post)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: error.message })
-    }
-})
-
-app.put('/posts/:id', async (req, res) => {
-  const { id } = req.params
-  const post = await Post.findByIdAndUpdate(id, req.body, { new: true })
-  res.status(200).json(post)
-})
-
-app.delete('/posts/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deleted = await Post.findByIdAndDelete(id)
-        if (deleted) {
-            return res.status(200).send("Post deleted")
-        }
-        throw new Error("Post not found")
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
