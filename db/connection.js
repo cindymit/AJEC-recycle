bea298ef74f5add327b5c50e14a68e252434e5a7
joelgirouxx <j.giroux9@gmail.com>
2021-05-28 09:17:18 -0400

    package-lock.json changed

--- db/connection.js
+++ db/connection.js
@@ -1,32 +1,37 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 let MONGODB_URI =
-  process.env.PROD_MONGODB ||
-  'mongodb://127.0.0.1:27017/postsDevDatabase'
+  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/postsDev Database";
 
 // Uncomment to debug Mongoose queries
 // mongoose.set('debug', true)
 
 // Will create indexes in MongoDB by default for faster queries
-mongoose.set('useCreateIndex', true)
+mongoose.set("useCreateIndex", true);
 
 // This is for Model.findByIdAndUpdate method, specifically the so that { new: true} is the default
 // Learn more: https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
-mongoose.set('returnOriginal', false)
+mongoose.set("returnOriginal", false);
 
 // Setup connection for MongoDB
 // https://mongoosejs.com/docs/connections.html#connections
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
-  .catch((error) => console.error('Error connecting to MongoDB: ', error.message))
+  .catch((error) =>
+    console.error("Error connecting to MongoDB: ", error.message)
+  );
 
 // Listen to MongoDB events
 // Learn more: https://mongoosejs.com/docs/connections.html#connection-events
-mongoose.connection.on('disconnected', () => console.log(`Disconnected from MongoDB!`))
+mongoose.connection.on("disconnected", () =>
+  console.log(`Disconnected from MongoDB!`)
+);
 
 // Listen to any errors while connected to MongoDB
 // Learn more: https://mongoosejs.com/docs/connections.html#error-handling
-mongoose.connection.on('error', (error) => console.error(`MongoDB connection error: ${error}`))
+mongoose.connection.on("error", (error) =>
+  console.error(`MongoDB connection error: ${error}`)
+);
 
 // Export the connection
-export default mongoose.connection
+export default mongoose.connection;
