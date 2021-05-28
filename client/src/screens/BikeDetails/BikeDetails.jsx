dce1514edfff02c02edf3865351bf2a5dedc0a92
Artem Furman <af@Artems-MBP.fios-router.home>
2021-05-28 16:37:08 -0400

    login

--- client/src/screens/BikeDetails/BikeDetails.jsx
+++ client/src/screens/BikeDetails/BikeDetails.jsx
@@ -1,58 +1,58 @@
-import React from 'react';
-import { useState, useEffect } from 'react'
-import './BikeDetails.css'
-import { Layout } from '../../components'
-import { getBike, deleteBike } from '../../services/products'
-import { useParams, Link } from 'react-router-dom'
+// import React from 'react';
+// import { useState, useEffect } from 'react'
+// import './BikeDetails.css'
+// import { Layout } from '../../components'
+// import { getBike, deleteBike } from '../../services/bikes'
+// import { useParams, Link } from 'react-router-dom'
 
 
-const BikeDetails = (props) => {
-  const [bike, setBike] = useState(null)
-  const [isLoaded, setLoaded] = useState(false)
-  const { id } = useParams()
+// const BikeDetails = (props) => {
+//   const [bike, setBike] = useState(null)
+//   const [isLoaded, setLoaded] = useState(false)
+//   const { id } = useParams()
 
-  useEffect(() => {
-    const fetchBike = async () => {
-      const product = await getBike(id)
-      setBike(product)
-      setLoaded(true)
-    }
-    fetchBike()
-  }, [id])
+//   useEffect(() => {
+//     const fetchBike = async () => {
+//       const bike = await getBike(id)
+//       setBike(bike)
+//       setLoaded(true)
+//     }
+//     fetchBike()
+//   }, [id])
 
-  if (!isLoaded) {
-    return <h1>Loading...</h1>
-  }
+//   if (!isLoaded) {
+//     return <h1>Loading...</h1>
+//   }
 
 
 
-  return (
-    <Layout user={props.user}>
-    <div className='bike-detail'>
-        <img
-          className='bike-detail-image'
-          src={bike.imgURL}
-          alt={bike.name}
-        />
-        <div className='detail'>
-          <div className='name'>{bike.name}</div>
-          <div className='price'>{`$${bike.price}`}</div>
-          <div className='description'>{bike.description}</div>
-          <div className='button-container'>
-            <Link className='edit-button' to={`/bikes/${bike._id}/edit`}>
-              Edit
-            </Link>
-            <button
-              className='delete-button'
-              onClick={() => deleteProduct(bike._id)}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-      </Layout>
-  );
-};
+//   return (
+//     <Layout user={props.user}>
+//     <div className='bike-detail'>
+//         <img
+//           className='bike-detail-image'
+//           src={bike.imgURL}
+//           alt={bike.name}
+//         />
+//         <div className='detail'>
+//           <div className='name'>{bike.name}</div>
+//           <div className='price'>{`$${bike.price}`}</div>
+//           <div className='description'>{bike.description}</div>
+//           <div className='button-container'>
+//             <Link className='edit-button' to={`/bikes/${bike._id}/edit`}>
+//               Edit
+//             </Link>
+//             <button
+//               className='delete-button'
+//               onClick={() => deleteBike(bike._id)}
+//             >
+//               Delete
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//       </Layout>
+//   );
+// };
 
-export default BikeDetails;
+// export default BikeDetails;
