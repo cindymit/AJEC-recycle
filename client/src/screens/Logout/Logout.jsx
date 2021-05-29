6fd9c52f051caf0097e1f1987b963bc9216b303e
ealtidor <emanuella.altidor@gmail.com>
2021-05-28 22:20:48 -0400

    Completed Logout Screen

--- client/src/screens/Logout/Logout.jsx
+++ client/src/screens/Logout/Logout.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useEffect } from 'react'
+import { signOut } from '../../services/users'
+import { useHistory } from 'react-router-dom'
 
-const Logout = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+const Logout = (props) => {
+  const { setUser } = props
+  const history = useHistory()
+
+  useEffect(() => {
+    const signOutUser = async () => {
+      await signOut()
+      setUser(null)
+      history.push('/')
+    }
+    signOutUser()
+  }, [history, setUser])
+
+  return ''
 };
 
 export default Logout;
