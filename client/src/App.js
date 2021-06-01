d24c86e3ba9ca77248d4b15a0b5181a22bc6828b
Artem Furman <af@Artems-MBP.fios-router.home>
2021-06-01 19:00:41 -0400

    bike details button CSS

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import CreateBike from "./screens/CreateBike/CreateBike";
 import Home from "./screens/Home/Home";
 import Login from "./screens/Login/Login";
 import Logout from "./screens/Logout/Logout";
-import SignUp from "./screens/SignUp/SignUp";
+import SignUp from "./screens/SignUp/SignUp.jsx";
 import { verifyUser } from "./services/users";
 import { Route, Switch, Redirect } from "react-router-dom";
 
