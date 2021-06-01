58b213660f7922b812efd21f359499d12004fc36
ealtidor <emanuella.altidor@gmail.com>
2021-06-01 11:13:02 -0400

    Minor Updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect} from 'react'
 import AllBikes from "./screens/AllBikes/AllBikes";
 import BikeDetails from "./screens/BikeDetails/BikeDetails";
 import BikeEdit from "./screens/BikeEdit/BikeEdit";
@@ -6,14 +7,27 @@ import Home from "./screens/Home/Home";
 import Login from "./screens/Login/Login";
 import Logout from "./screens/Logout/Logout";
 import SignUp from "./screens/SignUp/SignUp";
+import {verifyUser} from "./services/users"
 import { Route, Switch } from "react-router-dom";
+
 import './App.css';
 
 function App() {
+  
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser()
+      user ? setUser(user) : setUser(null)
+    }
+    fetchUser()
+  }, [])
+
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/"  component={Home} />
         <Route path="/all-bikes" component={AllBikes} />
         <Route path="/create-bike" component={CreateBike} />
         <Route exact path="/bikes/:id" component={BikeDetails} />
