8d547a6264a83abe7e4372e35bdce720d25a14cc
Cindy Mit <cindymit89@gmail.com>
2021-06-01 11:57:33 -0400

    Modified CSS for AllBikes, CreateBike, and Search

--- client/src/components/Bikes/Bikes.jsx
+++ client/src/components/Bikes/Bikes.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import Link from "react";
 import "./Bikes.css";
 
 const Bikes = (props) => {
   const allTheBikes = props.bikes.map((bike) => {
     return (
-        <div className="bike-container">
-          <div className="bike-post">
-            <img src={bike.imgURL} alt="bike_image" className="bike-img" />
-            <p>Brand: {bike.brand}</p>
-            <p>Category: {bike.category}</p>
-            <p>Condition: {bike.condition}</p>
-            <p>Price: ${bike.price}</p>
-            <p>Seller: {bike.sellerName}</p>
-            <p>Contact Info: {bike.sellerEmail}</p>
-          </div>
+      <div className="bike-container">
+        {/* <Link to={`/bikes/${props.id}`} className="bike-link"> */}
+        <div className="bike-post">
+          <img src={bike.imgURL} alt="bike_image" className="bike-img" />
+          <p><strong>Brand:</strong> {bike.brand}</p>
+          <p><strong>Category:</strong> {bike.category}</p>
+          <p><strong>Condition:</strong> {bike.condition}</p>
+          <p><strong>Price:</strong> ${bike.price}</p>
+          <p><strong>Seller:</strong> {bike.sellerName}</p>
+          <p><strong>Contact Info:</strong> {bike.sellerEmail}</p>
         </div>
+        {/* </Link> */}
+      </div>
     );
   });
-  return <div>{allTheBikes}</div>;
+  return <div className="allbikesdiv">{allTheBikes}</div>;
 };
 
 export default Bikes;
